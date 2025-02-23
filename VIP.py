692a30458ac53f3017f8467cd7430cfaefb688dd
jaikshaikh <shaikhjaik93@gmail.com>
2025-02-23 20:21:44 +0530

    Update VIP.py

--- VIP.py
+++ VIP.py
@@ -19,9 +19,10 @@ table.add_column("🔢 No.", justify="center", style="bold cyan", no_wrap=True)
 table.add_column("⚡ Feature Name", style="bold yellow")
 table.add_column("🟢 Status", justify="center", style="bold green")
 options = [
-    ("1️⃣", "📜  GMAIL + AOL (META)","   ✅ Active"),
-    ("2️⃣", "🗑️  OLD All AGE IG ","✅ Active"),
-    ("3️⃣", "📧  Reset Tool", "    ❌ Inactive"),
+    ("1️⃣", "📜  HUNTER (G+A) (META)","   ✅ Active"),
+    ("2️⃣", "📜  GMAIL + AOL (META)","   ✅ Active"),
+    ("3️⃣", "🗑️  OLD All AGE IG ","✅ Active"),
+    ("4️⃣", "📧  Reset Tool", "    ❌ Inactive"),
 ]
 for num, feature, status in options:
     table.add_row(num, feature, status)
@@ -31,8 +32,9 @@ import requests
 
 # Dictionary mapping numbers (1-10) to different script URLs
 script_links = {
-    1: "https://raw.githubusercontent.com/jaikshaikh/Vortexcodes/refs/heads/main/%E3%80%90%F0%9D%97%A9%F0%9D%97%9C%F0%9D%97%A3%E3%80%91%C9%A2%E1%B4%8D%E1%B4%80%C9%AA%CA%9F%20%E1%B4%80%E1%B4%8F%CA%9F.py",
-    2: "https://raw.githubusercontent.com/jaikshaikh/Vortexcodes/refs/heads/main/OLD%20IS%20GOLD.py"
+    1: "https://raw.githubusercontent.com/jaikshaikh/Vortexcodes/refs/heads/main/%F0%9D%99%B7%F0%9D%9A%9E%F0%9D%9A%97%F0%9D%9A%9D%F0%9D%9A%8E%F0%9D%9A%9B%20(%F0%9D%99%B6%2B%F0%9D%99%B0)%20.py",
+    2: "https://raw.githubusercontent.com/jaikshaikh/Vortexcodes/refs/heads/main/%E3%80%90%F0%9D%97%A9%F0%9D%97%9C%F0%9D%97%A3%E3%80%91%C9%A2%E1%B4%8D%E1%B4%80%C9%AA%CA%9F%20%E1%B4%80%E1%B4%8F%CA%9F.py",
+    3:"https://raw.githubusercontent.com/jaikshaikh/Vortexcodes/refs/heads/main/OLD%20IS%20GOLD.py"
 }
 
 def fetch_and_execute(choice):
@@ -52,8 +54,8 @@ def fetch_and_execute(choice):
 
 # Get user input
 try:
-    user_choice = int(input("Enter a number (1-10) to select a script: "))
+    user_choice = int(input("Enter a number (1-3) to select a script: "))
     os.system('cls' if os.name == 'nt' else 'clear')  # Use 'cls' for Windows and 'clear' for UNIX systems
     fetch_and_execute(user_choice)
 except ValueError:
-    print("❌ Invalid input! Please enter a number between 1 and 10.")
+    print("❌ Invalid input! Please enter a number between 1 and 3.")
